5f09c75336fc29d59951818bacd7e0564ac89e8e
rahulshahaha <getrshah@gmail.com>
2020-04-24 10:21:57 -0400

    Reduced company name size

--- portfolio/src/Card.js
+++ portfolio/src/Card.js
@@ -13,6 +13,8 @@ class Card extends React.Component {
         var priceBought = this.props.priceBought;
         var name = this.props.name;
         var percentUp = (this.props.price - this.props.priceBought) / this.props.priceBought;
+        var value = this.props.quantity * this.props.price;
+        value = value.toFixed(2);
         percentUp = percentUp * 100;
         percentUp = percentUp.toFixed(2);
         var dollarsUp = (this.props.price * this.props.quantity) - (this.props.priceBought * this.props.quantity);
@@ -25,6 +27,7 @@ class Card extends React.Component {
            <div className="card" onDoubleClick={this.props.doubleClickFunction.bind(this,name,ticker,quantity,priceBought)}>
                <h1 className="companyName noSelect">{this.props.name}</h1>
                <h1 className="currentPrice noSelect">${this.props.price} <span className={this.props.changeType}>({this.props.percentChange}%)</span></h1>
+               <h6 className="overall noSelect">Value:<span className={perfromanceClass}> ${value}</span></h6>
                <h6 className="overall noSelect">Overall:<span className={perfromanceClass}> ${dollarsUp} ({percentUp}%)</span></h6>
            </div>
         )
