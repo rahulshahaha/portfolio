08d94129c87cc5d37b5844331605c6f05ffae4fa
rahulshahaha <getrshah@gmail.com>
2020-05-04 11:11:37 -0400

    sorted cards

--- portfolio/src/Components/Deck.js
+++ portfolio/src/Components/Deck.js
@@ -7,9 +7,24 @@ class Deck extends React.Component {
     generateCards = () =>{
         var func = this.props.doubleClickFunction;
         var currentHoldings = this.props.currentHoldings;
+
+
         if(currentHoldings == null){
             return null;
         }
+
+        currentHoldings.sort(function(a,b){
+            //return a.attributes.OBJECTID - b.attributes.OBJECTID;
+            var returnValue = 0;
+            if(a.value === b.value)
+                returnValue = 0;
+            if(a.value < b.value)
+                returnValue = 1;
+            if(a.value > b.value)
+                returnValue = -1;
+
+           return returnValue
+        });
         var cardsList = this.props.currentHoldings.map(function(s){
             return <Card key={s.key} doubleClickFunction={func} holding={s} />;
         });
